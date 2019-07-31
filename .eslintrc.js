6a36ca57256e63febb12cbda540908e3e0b371a2
kk-tec <2090202057@qq.com>
2019-07-31 23:31:17 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
     extends: 'airbnb',
-    ecmaFeatures: {
-        jsx: true,
-        modules: true
-    },
+    // ecmaFeatures: {
+    //     jsx: true,
+    //     modules: true
+    // },
     env: {
         browser: true,
-        node: true,
-        es6: true
+        // node: true,
+        // es6: true
     },
     parser: 'babel-eslint',
     rules: {
         'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }]
     },
-    plugins: ['react']
+    // plugins: ['react']
 }
--- src/router.js
+++ src/router.js
@@ -11,23 +11,23 @@ function Login() {
 
 function AppRouter() {
   return (
-      <Router>
-          <div>
-              <nav>
-                  <ul>
-                      <li>
-                          <Link to="/">Home</Link>
-                        </li>
-                      <li>
-                          <Link to="/login/">Login</Link>
-                        </li>
-                    </ul>
-                </nav>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login/">Login</Link>
+            </li>
+          </ul>
+        </nav>
 
-              <Route path="/" exact component={Index} />
-              <Route path="/login/" component={Login} />
-            </div>
-        </Router>
+        <Route path="/" exact component={Index} />
+        <Route path="/login/" component={Login} />
+      </div>
+    </Router>
   );
 }
 
